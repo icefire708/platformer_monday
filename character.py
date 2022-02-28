fc3043311f9b134704cb95c41ad417aa55663402
BuildTools <unconfigured@null.spigotmc.org>
2022-02-28 20:59:44 +0300

    first commit

---
+++ character.py
@@ -0,0 +1,6 @@
+class Character:
+    def __init__(self) -> None:
+        self.HP = 10
+
+    def attack(self) -> None:
+        print('Я атаковал!!!')
---
+++ main.py
@@ -0,0 +1,9 @@
+from character import Character
+
+def main() -> None:
+    print('Game started!')
+    player = Character()
+    player.attack()
+
+if __name__ == '__main__':
+    main()
